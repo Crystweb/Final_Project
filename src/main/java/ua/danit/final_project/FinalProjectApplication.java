c6e1d9bd991e5c449e6fe1786c1c5c556ffa7e61
Rostyslav <31209221+warfawl@users.noreply.github.com>
2018-07-31 21:52:43 +0300

    initial commit - empty spring app (#4)

---
+++ src/main/java/ua/danit/final_project/FinalProjectApplication.java
@@ -0,0 +1,12 @@
+package ua.danit.final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinalProjectApplication.class, args);
+  }
+}
