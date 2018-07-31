9c6a24e9911c7d69746092ff21e575c53c814524
Rostyslav Barmakov <warfawl@gmail.com>
2018-07-31 21:44:34 +0300

    initial commit - empty spring app

---
+++ src/test/java/ua/danit/final_project/FinalProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.danit.final_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalProjectApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
