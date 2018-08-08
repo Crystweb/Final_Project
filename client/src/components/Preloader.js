f638fba5bf2f5a86970ac2b4abc14a2bc383886d
Artem <artyomtymchuk@gmail.com>
2018-08-08 23:01:03 +0300

    add preloader on Comments page

--- client/src/components/Preloader.js
+++ client/src/components/Preloader.js
@@ -4,7 +4,7 @@ import '../styles/preloader.css'
 const Preloader = () => {
   return (
     <div className="load">
-      <div className="container">
+      <div className="load__container">
         <div className="load__item"></div>
         <div className="load__item"></div>
         <div className="load__item"></div>
--- client/src/pages/comments/Comments.js
+++ client/src/pages/comments/Comments.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import '../../styles/comments.css'
 import settings from '../../constants/settings'
 import axios from 'axios'
+import Preloader from '../../components/Preloader'
 
 class Comments extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      userName: '',
-      userMessage: '',
-      messageDate: ''
+      userName: null,
+      userMessage: null,
+      messageDate: null
     }
   }
 
@@ -28,13 +29,21 @@ class Comments extends Component {
   }
 
   render () {
-    return (
-      <div className="container">
-        <h2>{this.state.userName}</h2>
-        <h1>{this.state.userMessage}</h1>
-        <h3>{this.state.messageDate}</h3>
-      </div>
-    )
+    if (!!this.state.userName && !!this.state.userMessage && !!this.state.messageDate) {
+      return (
+        <div className="container">
+          <h2>{this.state.userName}</h2>
+          <h1>{this.state.userMessage}</h1>
+          <h3>{this.state.messageDate}</h3>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Preloader/>
+        </div>
+      )
+    }
   }
 }
 
