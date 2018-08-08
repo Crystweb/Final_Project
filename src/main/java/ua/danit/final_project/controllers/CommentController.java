fab95215837c2a71294cc3ad34ce8ad16ae72b3b
Artem <artyomtymchuk@gmail.com>
2018-08-08 22:03:38 +0300

    fix for Travis

--- src/main/java/ua/danit/final_project/controllers/CommentController.java
+++ src/main/java/ua/danit/final_project/controllers/CommentController.java
@@ -9,21 +9,21 @@ import ua.danit.final_project.services.CommentService;
 @RestController
 public class CommentController {
 
-    private final CommentService commentService;
+  private final CommentService commentService;
 
-    @Autowired
-    public CommentController(CommentService commentService) {
-        this.commentService = commentService;
-    }
+  @Autowired
+  public CommentController(CommentService commentService) {
+    this.commentService = commentService;
+  }
 
-    @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Comment addComment(@RequestParam("uid") Long uid,
-                              @RequestParam("message") String message) {
-        return commentService.addComment(uid, message);
-    }
+  @RequestMapping(value = "/comment", method = RequestMethod.POST)
+  public Comment addComment(@RequestParam("uid") Long uid,
+                            @RequestParam("message") String message) {
+    return commentService.addComment(uid, message);
+  }
 
-    @RequestMapping(value = "/comment", method = RequestMethod.GET)
-    public Comment getLast() {
-        return commentService.getLastComment();
-    }
+  @RequestMapping(value = "/comment", method = RequestMethod.GET)
+  public Comment getLast() {
+    return commentService.getLastComment();
+  }
 }
--- src/main/java/ua/danit/final_project/entities/Comment.java
+++ src/main/java/ua/danit/final_project/entities/Comment.java
@@ -5,15 +5,15 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
-import javax.persistence.GenerationType;
-import javax.persistence.FetchType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
@@ -37,8 +37,6 @@ public class Comment implements Serializable {
   @JsonFormat(pattern = "dd-MM-yyyy hh:mm")
   @Column(name = "c_date", nullable = false)
   private Timestamp date;
-
-
 
 
 }
