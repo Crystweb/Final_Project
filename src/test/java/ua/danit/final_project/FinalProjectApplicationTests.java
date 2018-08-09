17f13d844d09e30df22cd5f1f0b2e926dfb6ecfe
Rostyslav Barmakov <warfawl@gmail.com>
2018-08-09 12:16:30 +0300

    added role service

--- src/test/java/ua/danit/final_project/FinalProjectApplicationTests.java
+++ src/test/java/ua/danit/final_project/FinalProjectApplicationTests.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ua.danit.final_project.entities.Role;
 import ua.danit.final_project.repositories.CommentRepo;
 import ua.danit.final_project.repositories.RoleRepo;
 import ua.danit.final_project.repositories.UserRepo;
+import ua.danit.final_project.services.RoleService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,6 +24,9 @@ public class FinalProjectApplicationTests {
 
   @Autowired
   CommentRepo commentRepo;
+
+  @Autowired
+  RoleService roleService;
 
   @Test
   public void contextLoads() {
@@ -37,4 +42,19 @@ public class FinalProjectApplicationTests {
     Assert.assertTrue(userRepo.findAll().size() > 1);
   }
 
+
+  @Test
+  public void newRoleSaved() {
+    Role actual = roleService.create("test");
+    Role expected = roleService.findRole("test");
+
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void roleRemoved() {
+    Role admin = roleService.remove("admin");
+
+    Assert.assertNotNull(admin);
+  }
 }
