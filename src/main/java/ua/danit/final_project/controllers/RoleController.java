1df7f9bb2eec0015b0c86665c78608030f00c113
Rostyslav Barmakov <warfawl@gmail.com>
2018-08-09 23:02:12 +0300

    split import

--- src/main/java/ua/danit/final_project/controllers/RoleController.java
+++ src/main/java/ua/danit/final_project/controllers/RoleController.java
@@ -1,6 +1,9 @@
 package ua.danit.final_project.controllers;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ua.danit.final_project.entities.Role;
 import ua.danit.final_project.services.RoleService;
 
