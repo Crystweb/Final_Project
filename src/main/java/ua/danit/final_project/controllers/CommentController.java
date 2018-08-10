8e752f35669e34b47f2b4ba4cb5c64267915e3bd
Romik9 <remont.moy-pc@yandex.ru>
2018-08-10 17:32:41 +0300

    TEST=TEST

--- src/main/java/ua/danit/final_project/controllers/CommentController.java
+++ src/main/java/ua/danit/final_project/controllers/CommentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ua.danit.final_project.entities.Comment;
 import ua.danit.final_project.services.CommentService;
 
-import ua.danit.final_project.services.Varsdeer;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
