6f0814e13ff610de31d02766c56ad1459b321889
Rostyslav Barmakov <warfawl@gmail.com>
2018-08-10 21:50:05 +0300

    removed unused import

---
+++ src/main/java/ua/danit/final_project/entities/WorkShift.java
@@ -0,0 +1,31 @@
+package ua.danit.final_project.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "work_shift")
+@Data
+public class WorkShift implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "r_id", nullable = false)
+  private Role role;
+
+  private Timestamp start;
+  private Timestamp end;
+}
--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS `task`;
+DROP TABLE IF EXISTS `location`;
+DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `user_work_shift`;
+DROP TABLE IF EXISTS `work_shift`;
 DROP TABLE IF EXISTS `comment`;
 DROP TABLE IF EXISTS `user`;
 DROP TABLE IF EXISTS `role`;
@@ -14,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `u_login` VARCHAR(32) NOT NULL UNIQUE,
   `u_password` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`r_id`) REFERENCES role(`id`)
+  FOREIGN KEY (`r_id`) REFERENCES `role`(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 CREATE TABLE IF NOT EXISTS `comment` (
@@ -26,3 +31,52 @@ CREATE TABLE IF NOT EXISTS `comment` (
   FOREIGN KEY (`u_id`) REFERENCES `user`(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
+CREATE TABLE IF NOT EXISTS `work_shift` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `r_id` BIGINT NOT NULL,
+  `start` TIME NOT NULL,
+  `end` TIME NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`r_id`) REFERENCES `role`(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `user_work_shift` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `u_id` BIGINT NOT NULL,
+  `w_shift_id` BIGINT NOT NULL,
+  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`u_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY (`w_shift_id`) REFERENCES `work_shift`(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `u_id` BIGINT,
+  `e_forename` VARCHAR(32) NOT NULL,
+  `e_surname` VARCHAR(32) NOT NULL,
+  `e_patronymic` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`u_id`) REFERENCES `user`(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `l_title` VARCHAR(50) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `t_author_u_id` BIGINT NOT NULL,
+  `t_assignee_u_id` BIGINT NOT NULL,
+  `l_id` BIGINT NOT NULL,
+  `t_status` ENUM ('ACTIVE', 'CLOSED', 'PROCESSING', 'FINISHED'),
+  `t_text` VARCHAR(500) NOT NULL,
+  `t_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `t_status_changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`t_assignee_u_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY (`t_author_u_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY (`l_id`) REFERENCES  `location`(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
