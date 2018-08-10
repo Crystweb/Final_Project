be22ec3c36fdaf230f6e322c81526493def5a9b1
dimasemenchenko <gararararara001@gmail.com>
2018-08-10 17:32:15 +0300

    button with a clear page

---
+++ client/src/components/Users.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Users = () => {
+    return (
+        <div className="container">
+            <div className="user_list">
+                <div className="user">
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Users
