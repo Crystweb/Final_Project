1869c314faea6d2b5a0c94cdf75d93487777a62b
Vlas <40273797+VlasD@users.noreply.github.com>
2018-08-12 12:55:42 +0300

    Add buttons in page tasks (#38)
    * Add page name_Tasks
    
    * Add buttons/href in page Tasks. Add style buttons.
    
    * Update App.scss
    
    * Add buttons/href in page Tasks. Add style buttons.
    
    * Add buttons/href in page Tasks. Add style buttons.
    
    * Add buttons/href in page Tasks. Add style buttons.
    
    * fix Employees.js
    
    * fix Home.js

---
+++ client/src/pages/tasks/MyTasks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class MyTasks extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="tasks">
+                    Page for MyTasks
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MyTasks;
---
+++ client/src/pages/tasks/TasksOfCyclic.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class TasksOfCyclic extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="tasks">
+                    Page for CycleTasks
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TasksOfCyclic;
