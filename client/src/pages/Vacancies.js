2bded9232caf248cbb258e732947030206d1720d
dimasemenchenko <gararararara001@gmail.com>
2018-08-14 19:10:35 +0300

    workink add vacancy button

--- client/src/pages/Vacancies.js
+++ client/src/pages/Vacancies.js
@@ -1,13 +1,61 @@
 import React, {Component} from 'react'
+import Modal from 'react-responsive-modal';
+import axios from 'axios';
 
 class Vacancies extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            position:'',
+            salary:'',
+            open: false
+        };
+        this.handlePositionChange = this.handlePositionChange.bind(this);
+        this.handleSalaryChange = this.handleSalaryChange.bind(this);
+    }
+
+    onOpenModal = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal = () => {
+        this.setState({open: false});
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        axios.post(`http://localhost:9000/vacancy`,{
+            position: this.state.position,
+            salary: this.state.salary
+        })
+    };
+
+    handlePositionChange(event) {
+        event.preventDefault();
+        this.setState({position: event.target.value});
+    }
+    handleSalaryChange(event) {
+        event.preventDefault();
+        this.setState({salary: event.target.value});
+    }
+
     render() {
+        const {open} = this.state;
         return (
-            <div className="container">
-                <nav className="navigation">
-                    <ul>
-                    </ul>
-                </nav>
+            <div className="button-container" id="button">
+                <button onClick={this.onOpenModal}>Добавить вакансию</button>
+                <Modal open={open} onClose={this.onCloseModal} center
+                       closeOnOverlayClick={true}>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Название должности:
+                            <input type="text" name={"position"} value={this.state.position} onChange={this.handlePositionChange}/>
+                            Зарплата:
+                            <input type="number" name={"salary"} value={this.state.salary} onChange={this.handleSalaryChange}/>
+                        </label>
+                        <input type="submit" value="Добавить"/>
+                    </form>
+                </Modal>
             </div>
         )
     }
--- src/main/java/ua/danit/final_project/controllers/VacancyController.java
+++ src/main/java/ua/danit/final_project/controllers/VacancyController.java
@@ -1,15 +1,12 @@
 package ua.danit.final_project.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.danit.final_project.entities.Vacancy;
 import ua.danit.final_project.services.VacancyService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/vacancy")
 public class VacancyController {
@@ -26,8 +23,7 @@ public class VacancyController {
   }
 
   @PostMapping
-  public Vacancy createVacancy(@RequestParam("position") String position,
-                                @RequestParam("salary") Integer salary) {
-    return vacancyService.create(position, salary, System.currentTimeMillis());
+  public Vacancy createVacancy(@RequestBody Vacancy vacancy) {
+    return vacancyService.create(vacancy.getPosition(), vacancy.getSalary(), System.currentTimeMillis());
   }
 }
