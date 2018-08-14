6c6ab61b893000a689f64ae894998d2640cd719b
Myroslav-Polishchuk <40273906+Myroslav-Polishchuk@users.noreply.github.com>
2018-08-14 11:10:12 +0300

    added entity, service, repository, controller of Employee (#39)
    * added entity, service, repository, controller of Employee
    
    * fixed code for Travis
    
    * fixed for SonarCloud
    
    * added position, telephone to Employee
    
    * delete test :(
    
    * fixed forename
    
    * error
    
    * Merge branches 'entity_repository_service_controller_employee' and 'master' of https://github.com/Romik9/Final_Project into entity_repository_service_controller_employee
    
    # Conflicts:
    #	src/main/resources/database/data.sql
    #	src/main/resources/database/schema.sql
    #	src/test/java/ua/danit/final_project/FinalProjectApplicationTests.java
    
    * removed unused import

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -3,6 +3,10 @@ INSERT INTO `role` (`r_name`) VALUES ('manager');
 
 INSERT INTO `user` (r_id, u_login, u_password) VALUES (1, 'rostyslav@mail.com','1');
 INSERT INTO `user` (r_id, u_login, u_password) VALUES (2, 'myroslav@mail.com','1');
+INSERT INTO `user` (r_id, u_login, u_password) VALUES (2, 'vas@mail.com','1');
+
+INSERT  INTO  `employee` (u_id, e_forename, e_surname, e_patronymic, e_telephone, e_position) VALUES (1, 'Vasyl', 'Kit', 'Vasylovich', '+380556221486', 'admin');
+INSERT  INTO  `employee` (u_id, e_forename, e_surname, e_patronymic, e_telephone, e_position) VALUES (2, 'Mykola', 'Kit', 'Vasylovich','+380995534219', 'boss');
 
 INSERT INTO `schedule` (`id`, `r_id`, `start`, `end`) VALUES (1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
