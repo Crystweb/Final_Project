e5ca10ee731004830839daa37c86e930751a28b4
Rostyslav Barmakov <warfawl@gmail.com>
2018-08-17 10:41:51 +0300

    removed controllers, entities, repositories, services, tests

--- src/main/java/ua/danit/final_project/repositories/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package ua.danit.final_project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.danit.final_project.entities.Employee;
-
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-}
--- src/main/java/ua/danit/final_project/repositories/WorkShiftRepo.java
+++
@@ -1,7 +0,0 @@
-package ua.danit.final_project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.danit.final_project.entities.WorkShift;
-
-public interface WorkShiftRepo extends JpaRepository<WorkShift, Long> {
-}
