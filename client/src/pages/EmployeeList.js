eac36085c9d7f28bcda8b93b51d90d1f2f749df6
Artem <artyomtymchuk@gmail.com>
2018-08-20 00:40:50 +0300

    fix EmployeeList

--- client/src/pages/EmployeeList.js
+++ client/src/pages/EmployeeList.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import Preloader from '../components/Preloader'
 
 class EmployeeList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('/employee')
+      .then(response => this.setState({users: response}))
+  }
+
   render () {
-    return (
-      <div className="container">
-        <div className=""></div>
-      </div>
-    )
+    if (!this.state.users.length) {
+      return (
+        <div>
+          <Preloader/>
+        </div>
+      )
+    } else {
+      return (
+        <ul className="employee-list">
+          {this.state.users.map(function (user) {
+            return (
+              <li key={user.id}>
+                <Link to="{'/users/' + user.id}">{user.name}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      )
+    }
   }
 }
 
