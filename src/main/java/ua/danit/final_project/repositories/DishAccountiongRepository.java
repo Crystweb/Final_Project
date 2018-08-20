dda622d8011c5430546bce077dc2bba3125b5d05
Rostyslav Barmakov <warfawl@gmail.com>
2018-08-20 23:27:39 +0300

    returned `_config.yml`

--- src/main/java/ua/danit/final_project/repositories/DishAccountiongRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.danit.final_project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.danit.final_project.entities.DishAccounting;
-
-@Repository
-public interface DishAccountiongRepository extends JpaRepository<DishAccounting, Long> {
-}
