cc4aba318a6e6bc50c072898c06d2a31ef65957e
Artem <artyomtymchuk@gmail.com>
2018-08-29 20:25:28 +0300

    fix java for travis

--- src/main/java/ua/danit/final_project/entities/WorkShift.java
+++ src/main/java/ua/danit/final_project/entities/WorkShift.java
@@ -5,7 +5,16 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.List;
