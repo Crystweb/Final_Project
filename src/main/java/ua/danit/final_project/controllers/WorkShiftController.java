513e3a6f404c86d7e9134410fea67cf708f76446
Artem <artyomtymchuk@gmail.com>
2018-08-29 20:15:53 +0300

    fix fot travis

--- src/main/java/ua/danit/final_project/controllers/WorkShiftController.java
+++ src/main/java/ua/danit/final_project/controllers/WorkShiftController.java
@@ -2,7 +2,13 @@ package ua.danit.final_project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ua.danit.final_project.entities.ShiftComment;
 import ua.danit.final_project.entities.WorkShift;
