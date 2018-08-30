31ddd48ece8dd01e317522c524d4a27ecd034e3a
Artem <artyomtymchuk@gmail.com>
2018-08-30 07:12:59 +0300

    fix

--- src/main/java/ua/danit/final_project/controllers/TestUserController.java
+++ src/main/java/ua/danit/final_project/controllers/TestUserController.java
@@ -1,6 +1,5 @@
 package ua.danit.final_project.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +11,9 @@ import ua.danit.final_project.entities.User;
 @RequestMapping("/user")
 public class TestUserController {
 
-@GetMapping
-public ResponseEntity<User> getStaticUser (){
-   return ResponseEntity.ok(StaticCollection.getUser());
+  @GetMapping
+  public ResponseEntity<User> getStaticUser() {
+    return ResponseEntity.ok(StaticCollection.getUser());
   }
 
 }
