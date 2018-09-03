66d2978aacc29d6946f4371cdfee7a55f4bca169
Artem <artyomtymchuk@gmail.com>
2018-09-03 21:21:24 +0300

    add input radio select

--- client/src/pages/shifts/Shifts.js
+++ client/src/pages/shifts/Shifts.js
@@ -11,7 +11,7 @@ class Shifts extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      view: 'manager'
+      view: null
     }
   }
 
@@ -21,7 +21,12 @@ class Shifts extends Component {
     })
   }
 
+  setPositionView (event) {
+    this.setState({view: event.target.value})
+  }
+
   render () {
+
     if (!this.props.lastComments) {
       return (
         <div>
@@ -36,13 +41,16 @@ class Shifts extends Component {
         for (let i = 0; i < currentComment.length; i++) {
           positionComments = currentComment.filter(position[i].title.contains(this.state.view))
         }
+        console.log(positionComments)
       }
       const selectPositionInputs = position.map(position =>
-        <p><input name="position" type='radio' key={position.id} value={position.title} on/>{position.title}</p>)
+        <p><input name="position" type='radio' key={position.id} value={position.title}/>{position.title}</p>)
       return (
         <div className="container">
           <section className="comments">
-            {selectPositionInputs}
+            <div onChange={this.setPositionView.bind(this)}>
+              {selectPositionInputs}
+            </div>
             {positionComments}
           </section>
           <nav className="navigation">
