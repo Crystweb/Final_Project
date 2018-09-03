e71e6a51f941cb6b6f8280037343dc7589d64aac
BOSS <myr.character@gmail.com>
2018-09-03 20:37:21 +0300

    delete workshift from java and sql

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -34,20 +34,13 @@ INSERT INTO `user_role` (u_id, r_id) VALUES (2,5);
 INSERT INTO `position` (p_title) VALUES ('manager');
 INSERT INTO `position` (p_title) VALUES ('admin');
 INSERT INTO `position` (p_title) VALUES ('managing director');
-INSERT INTO `position` (p_title) VALUES ('cleaner');
-
 
 INSERT INTO `employee`(u_id, p_id, e_forename, e_surname, e_patronymic, e_phone_number, e_info) VALUES (1,1,'Vasyl', 'Vasyliv', 'Vasylovich', '4509654345', 'blablabla1');
 INSERT INTO `employee`(u_id, p_id, e_forename, e_surname, e_patronymic, e_phone_number, e_info) VALUES (2,2,'Petro', 'Petriv',  'Petrovych',  '094586403', 'blablabla2');
 
-
 INSERT INTO `schedule` (p_id, start, end) VALUES (2,  '10:00:00', '21:00:00' );
 INSERT INTO `schedule` (p_id, start, end) VALUES (2,  '21:00:00', '10:00:00' );
 INSERT INTO `schedule` (p_id, start, end) VALUES (1,  '08:00:00', '20:00:00' );
-
-INSERT INTO `work_shift` (position_id, start, end, date) VALUES (1, '23:30:10', '00:40:10', CURRENT_TIMESTAMP);
-INSERT INTO `work_shift` (position_id, start, end, date) VALUES (2, '10:30:10', '15:30:10', CURRENT_TIMESTAMP);
-INSERT INTO `work_shift` (position_id, start, end, date) VALUES (2, '13:30:10', '16:30:10', CURRENT_TIMESTAMP);
 
 INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (1, 'message to admin', CURRENT_TIMESTAMP);
 INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (1, 'Hello, admin', CURRENT_TIMESTAMP);
@@ -58,33 +51,33 @@ INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Hello, Edward 
 INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (1, 'Прибрати в кімнаті номер 23', CURRENT_TIMESTAMP);
 INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (2, 'Викликати таксі на 7:00', CURRENT_TIMESTAMP);
 INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Команда Шахтар виїжджає 20 січня', CURRENT_TIMESTAMP);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Передзвонити клієнту о 19 годині', CURRENT_TIMESTAMP * 2);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'В кімнаті 31 зламалася дверна ручка – викликати майстра на ранок', CURRENT_TIMESTAMP * 2);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Попередження про відключення світла з 20 по 23 години', CURRENT_TIMESTAMP * 2);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Партія продуктів затримається на 3 години', CURRENT_TIMESTAMP * 2);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Морозилка перестала працювати', CURRENT_TIMESTAMP * 2);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Перевірити кімнату номер 44 після генерального прибирання', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 121 поламка крану', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 22 погано прибрана кімната', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'На барі біля рецепції закінчилася горілка «Фінляндія»', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчується пральний порошок', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Залишилося 2 вільні номери протягом 3 днів', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчилися лампочки', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Сім’я з 18  кімнати просить переселити в інший номер де тихіший.', CURRENT_TIMESTAMP * 3);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 48 просять обігрівач', CURRENT_TIMESTAMP * 4);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Розбита попільничка в номері 40', CURRENT_TIMESTAMP * 4);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчився туалетний папір в кімнатах 25, 16, 33', CURRENT_TIMESTAMP * 4);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'В кімнаті 47 зламали міні-бар', CURRENT_TIMESTAMP * 4);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Мешканці 23 кімнати зловживають оковитою та стають буйними', CURRENT_TIMESTAMP * 4);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 55 замовила сніданок на 6:00', CURRENT_TIMESTAMP * 4);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 35 просили не турбувати 3 дні', CURRENT_TIMESTAMP * 5);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Зламалися вимикачі на складі', CURRENT_TIMESTAMP * 5);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Завтра приїде пожежна перевірка', CURRENT_TIMESTAMP * 5);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'На ресепшені закінчуються канцтовари ', CURRENT_TIMESTAMP * 6);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Потрібно оновити розмітку на автостоянці.', CURRENT_TIMESTAMP * 6);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Пропав wi-fi на третьому поверсі', CURRENT_TIMESTAMP * 6);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Завтра приїдуть англомовні клієнти', CURRENT_TIMESTAMP * 7);
-INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчуються цукерки на ресепшині', CURRENT_TIMESTAMP * 7);
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Передзвонити клієнту о 19 годині', TIMESTAMPADD(DAY, -1, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'В кімнаті 31 зламалася дверна ручка – викликати майстра на ранок', TIMESTAMPADD(DAY, -1, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Попередження про відключення світла з 20 по 23 години', TIMESTAMPADD(DAY, -1, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Партія продуктів затримається на 3 години', TIMESTAMPADD(DAY, -1, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Морозилка перестала працювати', TIMESTAMPADD(DAY, -1, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Перевірити кімнату номер 44 після генерального прибирання', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 121 поламка крану', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 22 погано прибрана кімната', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'На барі біля рецепції закінчилася горілка «Фінляндія»', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчується пральний порошок', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Залишилося 2 вільні номери протягом 3 днів', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчилися лампочки', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Сім’я з 18  кімнати просить переселити в інший номер де тихіший.', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 48 просять обігрівач', TIMESTAMPADD(DAY, -3, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Розбита попільничка в номері 40', TIMESTAMPADD(DAY, -3, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчився туалетний папір в кімнатах 25, 16, 33', TIMESTAMPADD(DAY, -3, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'В кімнаті 47 зламали міні-бар', TIMESTAMPADD(DAY, -3, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Мешканці 23 кімнати зловживають оковитою та стають буйними', TIMESTAMPADD(DAY, -3, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 55 замовила сніданок на 6:00', TIMESTAMPADD(DAY, -3, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Кімната 35 просили не турбувати 3 дні', TIMESTAMPADD(DAY, -4, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Зламалися вимикачі на складі', TIMESTAMPADD(DAY, -4, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Завтра приїде пожежна перевірка', TIMESTAMPADD(DAY, -4, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'На ресепшені закінчуються канцтовари ', TIMESTAMPADD(DAY, -5, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Потрібно оновити розмітку на автостоянці.', TIMESTAMPADD(DAY, -5, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Пропав wi-fi на третьому поверсі', TIMESTAMPADD(DAY, -5, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Завтра приїдуть англомовні клієнти', TIMESTAMPADD(DAY, -6, CURRENT_TIMESTAMP));
+INSERT INTO `shift_comment` (u_id, c_message, c_date) VALUES (3, 'Закінчуються цукерки на ресепшині', TIMESTAMPADD(DAY, -6, CURRENT_TIMESTAMP));
 
 
 INSERT INTO `shift_comment_position` (comment_id, position_id) VALUES (1, 1);
@@ -225,5 +218,3 @@ INSERT INTO `dish_accounting` (d_id, l_id, u_id, d_delta, date) VALUES (3, 3, 3,
 INSERT INTO `dish_comment` (u_id, d_id, c_message, c_date) VALUES (1, 1, 'broke dishes', CURRENT_TIMESTAMP);
 INSERT INTO `dish_comment` (u_id, d_id, c_message, c_date) VALUES (2, 2, 'broke spoons', CURRENT_TIMESTAMP);
 INSERT INTO `dish_comment` (u_id, d_id, c_message, c_date) VALUES (3, 3, 'broke forks', CURRENT_TIMESTAMP);
-
-INSERT INTO `work_shift` (position_id, start, end, date) VALUES (1, '00:00:00', '18:00:00', CURRENT_TIMESTAMP);
