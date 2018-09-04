931694aed59c845e968ca58d30db006ec33f618f
akoziulia <akoziulia@cogniance.com>
2018-09-04 20:42:11 +0300

    Add date and commentFilter

--- client/src/components/PositionButtons.js
+++ client/src/components/PositionButtons.js
@@ -21,17 +21,26 @@ class PositionButtons extends Component {
 
   render () {
     const {position, comments} = this.props
-    let positionComments = comments.map(comment =>
-
-      <li key={comment.id} style={{marginBottom: 15}}>
-        <h5>{comment.forename} {comment.surname}</h5>
-        <h3>{comment.text}</h3>
-        <h3>{comment.authorPosition}</h3>
-      </li>
-    )
+    let positionComments = comments
+      .filter(comment => comment.positions.includes(this.state.view))
+      .map(comment =>
+        <li key={comment.id} style={{marginBottom: 15}}>
+          <h5>{comment.forename} {comment.surname}</h5>
+          <h3>{comment.text}</h3>
+          <h3>{comment.authorPosition}</h3>
+          <h6>{new Date(comment.date).toDateString()}</h6>
+        </li>
+      )
     console.log(positionComments)
     const selectPositionInputs = position.map(position =>
-      <li key={position.id}><input name="position" type='radio' value={position.title}/>{position.title}</li>)
+      <li key={position.id}>
+        <input name="position"
+               type='radio'
+               checked={this.state.view === position.title}
+               value={position.title}/>
+        {position.title}
+      </li>
+    )
     return (
       <section className="comments">
         <div style={{marginBottom: 10}} onChange={this.setPositionView.bind(this)}>
