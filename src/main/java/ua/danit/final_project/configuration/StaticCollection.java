c94d9f8dbf0d955b5186f740775badc802433c64
a.tymchuk <37867603+ArtemTymchuk@users.noreply.github.com>
2018-09-05 17:27:12 +0300

    Read comments from back in history and main comments page. Add schedules and positions to Redux (#96)
    * add input radio
    
    * add input radio select
    
    * add data
    
    * add PositionButtons to commetnsHistory
    
    * add radioButtons 0.0.2
    
    * for Myroslav
    
    * Add date and commentFilter
    
    * rename PositionButtons
    
    * add Schedules
    
    * fix
    
    * fix 1
    
    * removed constraint from employee table (unique user-position)
    
    * removed test for static user

--- src/main/java/ua/danit/final_project/configuration/StaticCollection.java
+++ src/main/java/ua/danit/final_project/configuration/StaticCollection.java
@@ -1,16 +1,21 @@
 package ua.danit.final_project.configuration;
 
+import ua.danit.final_project.entities.Position;
 import ua.danit.final_project.entities.User;
 
 public class StaticCollection {
 
   private static User user;
+  private static Position position;
 
   static {
+    position = new Position();
+    position.setTitle("admin");
     user = new User();
     user.setId(1L);
     user.setLogin("Artem");
     user.setPassword("pwd");
+    user.setPosition(position);
   }
 
   public static User getUser() {
