26fd5cba0918d2b7905f89cb43cdcbcb9dacd9c1
Romik9 <remont.moy-pc@yandex.ru>
2018-09-06 16:29:59 +0300

    edit Vacancy Controller

--- src/main/java/ua/danit/final_project/controllers/VacancyController.java
+++ src/main/java/ua/danit/final_project/controllers/VacancyController.java
@@ -1,22 +1,18 @@
 package ua.danit.final_project.controllers;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import ua.danit.final_project.configuration.StaticCollection;
-import ua.danit.final_project.entities.User;
 import ua.danit.final_project.entities.Vacancy;
 import ua.danit.final_project.services.crud.VacancyService;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/vacancy")
 public class VacancyController {
     private final VacancyService vacancyService;
-    private User user = StaticCollection.getUser();
     public VacancyController(VacancyService vacancyService) {
         this.vacancyService = vacancyService;
     }
@@ -28,7 +24,6 @@ public class VacancyController {
 
     @PostMapping
     public Vacancy createVacancy(@RequestBody Vacancy vacancy){
-        vacancy.setUser(user);
         return  vacancyService.save(vacancy);
     }
 }
