3eaaf2bc5e8d52a2c21685048cc6968ca25a783e
Myroslav Polischuk <mpolischuk@l320st05.di.lan>
2018-09-08 14:43:09 +0300

    f

--- src/main/java/ua/danit/final_project/controllers/WorkShiftController.java
+++ src/main/java/ua/danit/final_project/controllers/WorkShiftController.java
@@ -11,13 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ua.danit.final_project.configuration.StaticCollection;
 import ua.danit.final_project.entities.Schedule;
 import ua.danit.final_project.dto.ShiftCommentDto;
 import ua.danit.final_project.entities.ShiftComment;
 import ua.danit.final_project.services.WorkCommentService;
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
