c31ec7ff86b9154dcdf51885992761ccb8e2d8a3
Rostyslav <31209221+warfawl@users.noreply.github.com>
2018-09-11 19:20:11 +0300

    Schedule services (#108)
    * removed test for static user
    
    * added abstract entity, schedule uuid, updated schema
    
    * added findByDate method
    
    * added controller
    
    * added test, fixed style
    
    * added remove by id
    
    * removed unused imports
    
    * fields annotated as Columns

--- src/main/java/ua/danit/final_project/FinalProjectApplication.java
+++ src/main/java/ua/danit/final_project/FinalProjectApplication.java
@@ -2,8 +2,10 @@ package ua.danit.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FinalProjectApplication {
   public static void main(String[] args) {
     SpringApplication.run(FinalProjectApplication.class, args);
