fdcd5d39b94251b1c01e7728630fc3f42814a299
Rostyslav Barmakov <warfawl@gmail.com>
2018-09-11 21:49:15 +0300

    added 'nosonar'

--- src/main/java/ua/danit/final_project/dto/ShiftCommentDto.java
+++ src/main/java/ua/danit/final_project/dto/ShiftCommentDto.java
@@ -13,21 +13,21 @@ import java.util.stream.Collectors;
 @Data
 public class ShiftCommentDto implements Serializable {
 
-  private Long id;
+  private Long id; // NOSONAR
 
-  private String text;
+  private String text; // NOSONAR
 
-  private Timestamp date;
+  private Timestamp date; // NOSONAR
 
-  private String forename;
+  private String forename; // NOSONAR
 
-  private String surname;
+  private String surname; // NOSONAR
 
-  private List<String> positions;
+  private List<String> positions; // NOSONAR
 
-  private String authorPosition;
+  private String authorPosition; // NOSONAR
 
-  private Long authorId;
+  private Long authorId; // NOSONAR
 
   public ShiftCommentDto(ShiftComment shiftComment) {
     this.id = shiftComment.getId();
