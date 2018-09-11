6e397a70f2fe44fcb73596b41e595a11b9fc229c
Vlas <40273797+VlasD@users.noreply.github.com>
2018-09-11 21:57:14 +0300

    Vacancy Dto (#105)
    * Vacancy Dto
    
    * fix service
    
    * fix scss
    
    * fix spaces in VC.java
    
    * fix *
    
    * added method reference instead of lambda expression
    
    * fix sonar
    
    * fix sonar v2

--- src/main/java/ua/danit/final_project/controllers/VacancyController.java
+++ src/main/java/ua/danit/final_project/controllers/VacancyController.java
@@ -5,18 +5,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import ua.danit.final_project.configuration.StaticCollection;
+import ua.danit.final_project.dto.VacancyDto;
 import ua.danit.final_project.entities.User;
 import ua.danit.final_project.entities.Vacancy;
 import ua.danit.final_project.services.crud.VacancyService;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/vacancy")
 public class VacancyController {
+
   private final VacancyService vacancyService;
   private User user = StaticCollection.getUser();
 
@@ -25,8 +26,11 @@ public class VacancyController {
   }
 
   @GetMapping
-  public List<Vacancy> getVacancies() {
-    return vacancyService.getOpenVacancies();
+  public List<VacancyDto> getVacanciesDto() {
+    return vacancyService.getOpenVacancies()
+            .stream()
+            .map(VacancyDto::new)
+            .collect(Collectors.toList());
   }
 
   @PostMapping
