af80ac72c019542b2620f7840b6b33561ab7e157
Romik9 <remont.moy-pc@yandex.ru>
2018-09-12 20:20:01 +0300

    edit Employee

--- client/src/components/VacanciesList.js
+++ client/src/components/VacanciesList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/VacanciesList.css'
 import axios from 'axios'
 import Preloader from './Preloader'
--- client/src/pages/EmployeeList.js
+++ client/src/pages/EmployeeList.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import Preloader from '../components/Preloader'
+// import Preloader from '../components/Preloader'
 
 class EmployeeList extends Component {
+
   constructor (props) {
     super(props)
     this.state = {
-      users: []
+        employee: []
     }
   }
 
   componentDidMount () {
     axios.get('/employee')
-      .then(response => this.setState({users: response}))
+      .then(response => this.setState({employee: response.data}))
   }
 
   render () {
-    if (!this.state.users.length) {
-      return (
-        <div>
-          <Preloader/>
-        </div>
-      )
-    } else {
-      return (
-        <ul className="employee-list">
-          {this.state.users.map(function (user) {
+    // if (!this.state.users.length) {
+    //         return (
+    //             {/*<div>*/}
+    //         {/*<Preloader/>*/}
+    //         {/*</div>*/}
+    //     )
+    //     } else {
             return (
-              <li key={user.id}>
-                <Link to="{'/users/' + user.id}">{user.name}</Link>
-              </li>
+                <ul className="employee-list">
+                    {this.state.employee.map(function (employee) {
+                        return (
+                            <li key={user.id}>
+                                <Link to="{'/employee/' + employee.id}">{employee.name}</Link>
+                            </li>
+
+                        )
+                    })}
+                </ul>
             )
-          })}
-        </ul>
-      )
     }
-  }
+  // }
 }
 
 export default EmployeeList
