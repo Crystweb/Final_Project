b193a78a2d82c8da8f2707a19f20eb0f780cac03
artem.tymchuk <artyomtymchuk@gmail.com>
2018-09-20 10:55:01 +0300

    fix travis3

--- client/src/components/VacanciesList.js
+++ client/src/components/VacanciesList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import '../styles/VacanciesList.css'
 import axios from 'axios'
 import Preloader from './Preloader'
