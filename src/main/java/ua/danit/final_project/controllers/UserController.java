8b833c1e5ab6953198b808dec3c063b1e862f44c
artem.tymchuk <artyomtymchuk@gmail.com>
2018-09-26 13:04:04 +0300

    fix

--- src/main/java/ua/danit/final_project/controllers/UserController.java
+++ src/main/java/ua/danit/final_project/controllers/UserController.java
@@ -14,20 +14,20 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserService userService;
+  private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
-    @GetMapping
-    public List<User> getUsers() {
-        return userService.getAll();
-    }
+  @GetMapping
+  public List<User> getUsers() {
+    return userService.getAll();
+  }
 
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable("id") Long id) {
-        return userService.getById(id);
-    }
+  @GetMapping("/{id}")
+  public User getUserById(@PathVariable("id") Long id) {
+    return userService.getById(id);
+  }
 }
