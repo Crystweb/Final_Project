bd175dacaa09027479ee8048166116bdcb4cff2c
Rostyslav Barmakov <warfawl@gmail.com>
2018-09-27 00:52:37 +0300

    added Task tests, refactored Schedule tests

--- src/test/java/ua/danit/final_project/ScheduleTests.java
+++ src/test/java/ua/danit/final_project/ScheduleTests.java
@@ -1,85 +1,76 @@
 package ua.danit.final_project;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import ua.danit.final_project.entities.Position;
 import ua.danit.final_project.entities.Schedule;
-import ua.danit.final_project.repositories.PositionRepository;
 import ua.danit.final_project.repositories.ScheduleRepository;
 import ua.danit.final_project.services.ScheduleService;
 
-import javax.persistence.EntityNotFoundException;
-import java.sql.Time;
+import java.util.LinkedList;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ScheduleTests {
 
+  private static long mockId = 0;
+
   @Autowired
   private ScheduleService scheduleService;
 
-  @Autowired
-  private PositionRepository positionRepository;
-
-  @Autowired
+  @MockBean
   private ScheduleRepository scheduleRepository;
 
   private Schedule schedule;
-  private Position position;
 
   @Before
   public void init() {
+    Mockito.when(scheduleRepository.save(any()))
+        .then((Answer<Schedule>) invocationMock -> {
+          Schedule argument = invocationMock.getArgument(0);
+          argument.setId(++mockId);
+          return argument;
+        });
+
+    Mockito.when(scheduleRepository.findAllByPositionAndExpired(any(), any()))
+        .then((Answer<List<Schedule>>) invocationMock -> {
+          List<Schedule> mock = new LinkedList<>();
+          mock.add(schedule);
+          return mock;
+        });
+
     schedule = new Schedule();
-    schedule.setStart(new Time(1534763270));
-    schedule.setEnd(new Time(1534764000));
-
-    position = new Position();
-    position.setPinnedToComment(true);
-    position.setTitle("mock");
-    position = positionRepository.save(position);
-
-    schedule.setPosition(position);
     schedule = scheduleService.create(schedule);
-  }
-
-  @After
-  public void purge() {
-    scheduleRepository.delete(schedule);
-    positionRepository.delete(position);
   }
 
   @Test
   public void scheduleAddedAndSaved() {
-    assertTrue(scheduleService.findByPosition(position).size() > 0);
+    assertNotEquals(0L, schedule.getId());
   }
 
   @Test
   public void scheduleExpiredAfterRemoval() {
-    Long id = scheduleRepository.findById(schedule.getId())
-        .orElseThrow(EntityNotFoundException::new)
-        .getId();
     schedule = scheduleService.remove(schedule);
-    Schedule removed = scheduleRepository.findById(id).orElse(null);
 
-    assertNotNull(removed);
     assertNotNull(schedule.getExpired());
   }
 
   @Test
   public void onUpdateNewCreatedAndOldExpired() {
-    schedule.setEnd(new Time(System.currentTimeMillis()));
-
     Long oldId = schedule.getId();
     Long newId = scheduleService.update(schedule).getId();
 
     assertNotEquals(oldId, newId);
-    scheduleRepository.deleteById(newId);
   }
 }
---
+++ src/test/java/ua/danit/final_project/TaskTests.java
@@ -0,0 +1,83 @@
+package ua.danit.final_project;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import ua.danit.final_project.entities.Task;
+import ua.danit.final_project.repositories.TaskRepository;
+import ua.danit.final_project.services.tasks.TaskService;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.any;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskTests {
+
+  private static long mockId = 0; // id counter to be set when mockRepository.save(obj) called
+
+  @Autowired
+  private TaskService taskService;
+
+  @MockBean
+  private TaskRepository mockTaskRepository;
+
+  private Task task;
+
+  @Before
+  public void init() {
+
+    task = new Task();
+    task.setStatus(Task.TaskStatus.OPENED);
+    task.setFrequency(Task.TaskFrequency.ONCE);
+    task.setPriority(1);
+    task.setMessage("this is task mock");
+
+    Mockito.when(mockTaskRepository.save(any(Task.class))).then((Answer<Task>) invocationOnMock -> {
+      Task argument = invocationOnMock.getArgument(0);
+      argument.setId(++mockId);
+      return argument;
+    });
+
+    Mockito.when(mockTaskRepository.findById(any())).thenReturn(Optional.of(task));
+  }
+
+  @Test
+  public void taskCreated() throws IOException {
+    Task newTask = taskService.create(task, null);
+
+    Assert.assertNotNull(newTask);
+    Assert.assertEquals(newTask.getMessage(), task.getMessage());
+  }
+
+  @Test
+  public void taskExpiredOnRemoval() throws IOException {
+    task = taskService.create(task, null);
+    Task removedTask = taskService.remove(task.getId());
+
+    Assert.assertEquals(removedTask.getStatus(), Task.TaskStatus.REMOVED);
+  }
+
+  @Test
+  public void findActiveReturnsOnlyActive() {
+    List<Task> allActive = taskService.findAllActive();
+    List<Task> filtered = allActive.stream()
+        .filter(t -> !t.getStatus().equals(Task.TaskStatus.CLOSED))
+        .filter(t -> !t.getStatus().equals(Task.TaskStatus.REMOVED))
+        .filter(t -> !t.getStatus().equals(Task.TaskStatus.REJECTED))
+        .collect(Collectors.toList());
+
+    Assert.assertEquals(allActive, filtered);
+  }
+}
