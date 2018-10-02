beb15051fe45c9947486cc64b17c81448049e94f
artem.tymchuk <artyomtymchuk@gmail.com>
2018-10-02 18:32:38 +0300

    Merge branch 'master' of https://github.com/Romik9/Final_Project into addTasks
    # Conflicts:
    #	client/src/pages/tasks/CreateNewTask.js
    #	src/main/java/ua/danit/final_project/controllers/TaskController.java

--- src/main/java/ua/danit/final_project/controllers/TaskController.java
+++ src/main/java/ua/danit/final_project/controllers/TaskController.java
@@ -1,5 +1,6 @@
 package ua.danit.final_project.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
