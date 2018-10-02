e38ca165cb426c65951820f0ede6a92608b26cc2
myr.character@gmail.com <myr.character@gmail.com>
2018-10-02 11:18:29 +0300

    almost ready SortedComments

--- client/src/components/SortedComments.js
+++ client/src/components/SortedComments.js
@@ -53,6 +53,188 @@ class PositionButtons extends Component {
                 return item;
             })
 
+        let arrayOfSchedules = [];
+        let copyOfSchedules = Object.assign({}, schedulesWithColors);
+
+        for (let key in copyOfSchedules) {
+            arrayOfSchedules.push(copyOfSchedules[key]);
+        }
+
+        arrayOfSchedules.map(item => {
+            let stringNumberStart = item.start.toString().substr(0, 2);
+            let stringNumberEnd = item.end.toString().substr(0, 2);
+
+            item.start = parseInt(stringNumberStart);
+            item.end = parseInt(stringNumberEnd);
+        }).sort( (item1, item2) => item2.start - item1.start);
+
+        let currentSchedule = arrayOfSchedules.pop();
+        let startTime = 0;
+        let areCommentsMustbeInsideSchedule = false;
+        let arrayOfReadyComments = [];
+
+        while (arrayOfSchedules.length >= 0) {
+            if (arrayOfReadyComments === null) {
+                return
+            }
+
+            if (!areCommentsMustbeInsideSchedule) {
+                arrayOfReadyComments.push(
+                    <div className="schedule-elem">
+                        <h2 className="schedule-elem__title">Без смены</h2>
+                        <ul className="comment-list">
+                            {comments
+                    .filter(comment => comment.authorPosition === this.state.view)
+                    .filter(comment => {
+                    let commentStartHours = new Date(comment.date).getHours();
+
+                    return commentStartHours >= startTime && commentStartHours < currentSchedule.start
+
+                }).sort( (comment1, comment2) => {
+                    let comment1StartHours = new Date(comment1.date).getHours();
+                    let comment2StartHours = new Date(comment2.date).getHours();
+
+                    return comment1StartHours - comment2StartHours;
+                }).map(comment => {
+                    const showActionButtons = comment.authorId === this.state.userId;
+
+                    return (
+                        <li className="comment-list__elem">
+                        <h3 className="comment-list__elem-title">
+                            {comment.forename + " " + comment.surname + ", " + comment.authorPosition}
+                        </h3>
+                        <div className="comment-list__elem-point">
+                            <div className="comment-list__elem-line"></div>
+                            <div className="point-big">
+                                <div className="point-small"></div>
+                            </div>
+                        </div>
+                        <h4 className="comment-list__elem-subtitle">
+                            17 июня в 21:45
+                        </h4>
+                        <p className="comment-list__elem-info">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus deserunt
+                            dignissimos earum est ex fuga hic id ipsa minus modi nam odit officiis perferendis,
+                            placeat provident similique ullam unde velit.
+                        </p>
+                        <div className="comment-list__elem-buttons">
+                            <a className="comment-list__elem-buttons-change" href="#">
+                                <img src="#" alt="#"/>
+                            </a>
+                            <a className="comment-list__elem-buttons-delete" href="#">
+                                <img src="#" alt="#"/>
+                            </a>
+                        </div>
+                    </li>
+                        )
+                    }
+                )
+
+            }
+                        </ul>
+                    </div>
+                )
+                areCommentsMustbeInsideSchedule = !areCommentsMustbeInsideSchedule;
+            } else {
+                comments.filter(comment => {
+                    let commentStartHours = new Date(comment.date).getHours();
+
+                    return  commentStartHours >= currentSchedule.start && commentStartHours < currentSchedule.end;
+                }).sort( (comment1, comment2) => {
+                    let comment1StartHours = new Date(comment1.date).getHours();
+                    let comment2StartHours = new Date(comment2.date).getHours();
+
+                    return comment1StartHours - comment2StartHours;
+                }).map(comment => comment.date);
+
+                areCommentsMustbeInsideSchedule = !areCommentsMustbeInsideSchedule;
+                startTime = currentSchedule.end;
+
+                if (arrayOfSchedules.length === 0) {
+                    break;
+                }
+
+                currentSchedule = arrayOfSchedules.pop();
+            }
+        }
+
+        comments.filter(comment => {
+            let commentStartHours = new Date(comment.date).getHours();
+
+            return commentStartHours >= startTime
+
+        }).sort( (comment1, comment2) => {
+            let comment1StartHours = new Date(comment1.date).getHours();
+            let comment2StartHours = new Date(comment2.date).getHours();
+
+            return comment1StartHours - comment2StartHours;
+        }).map(comment => comment.date)
+
+
+
+
+
+
+        // comments.filter(comment => {
+        //     let commentStartHours = new Date(comment.date).getHours();
+        //
+        //     return commentStartHours >= startTime && commentStartHours < currentSchedule.start
+        //
+        // }).sort( (comment1, comment2) => {
+        //     let comment1StartHours = new Date(comment1.date).getHours();
+        //     let comment2StartHours = new Date(comment2.date).getHours();
+        //
+        //     return comment1StartHours - comment2StartHours;
+        // }).map(comment => comment.date);
+        //
+        // //comments true
+        //
+        // comments.filter(comment => {
+        //     let commentStartHours = new Date(comment.date).getHours();
+        //
+        //     return  commentStartHours >= currentSchedule.start && commentStartHours < currentSchedule.end;
+        // }).sort( (comment1, comment2) => {
+        //     let comment1StartHours = new Date(comment1.date).getHours();
+        //     let comment2StartHours = new Date(comment2.date).getHours();
+        //
+        //     return comment1StartHours - comment2StartHours;
+        // }).map(comment => comment.date)
+        //
+        // areCommentsMustbeInsideSchedule = !areCommentsMustbeInsideSchedule;
+        // startTime = currentSchedule.end;
+        //
+        // if (arrayOfSchedules.length === 0) {
+        //     break;
+        // }
+        //
+        // currentSchedule = arrayOfSchedules.pop();
+        //
+        // //after end while
+        //
+        // comments.filter(comment => {
+        //     let commentStartHours = new Date(comment.date).getHours();
+        //
+        //     return commentStartHours >= startTime
+        //
+        // }).sort( (comment1, comment2) => {
+        //     let comment1StartHours = new Date(comment1.date).getHours();
+        //     let comment2StartHours = new Date(comment2.date).getHours();
+        //
+        //     return comment1StartHours - comment2StartHours;
+        // }).map(comment => comment.date)
+
+
+
+
+
+
+        console.log(arrayOfSchedules);
+        console.log(comments)
+
+
+
+
+
         const timelineEvents = [];
 
         for (let i = new Date().getHours(); i >= 0; i--) {
