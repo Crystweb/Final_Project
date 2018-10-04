8ed8acbb430af314dd8918cb6f5cef34d9a5f982
Rostyslav Barmakov <warfawl@gmail.com>
2018-10-04 15:44:56 +0300

    style fix

--- src/main/java/ua/danit/final_project/controllers/exceptions/GeneralExceptionHandler.java
+++ src/main/java/ua/danit/final_project/controllers/exceptions/GeneralExceptionHandler.java
@@ -16,36 +16,36 @@ public class GeneralExceptionHandler {
   private final Logger logger = LoggerFactory.getLogger(GeneralExceptionHandler.class);
 
   @ExceptionHandler({RuntimeException.class})
-  public ResponseEntity<ErrorResponse> handleRuntime(RuntimeException e) {
-    logger.error(e.getMessage(), e);
+  public ResponseEntity<ErrorResponse> handleRuntime(RuntimeException exception) {
+    logger.error(exception.getMessage(), exception);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(new ErrorResponse("Oops! Something went wrong...", 500));
   }
 
   @ExceptionHandler({IOException.class})
-  public ResponseEntity<ErrorResponse> handleIO(IOException e) {
-    logger.error(e.getMessage(), e);
+  public ResponseEntity<ErrorResponse> handleIO(IOException exception) {
+    logger.error(exception.getMessage(), exception);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(new ErrorResponse("Failed to upload file.", 500));
   }
 
   @ExceptionHandler({EntityNotFoundException.class})
-  public ResponseEntity<ErrorResponse> handleNotFound(EntityNotFoundException e) {
-    logger.warn(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleNotFound(EntityNotFoundException exception) {
+    logger.warn(exception.getMessage());
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
         .body(new ErrorResponse("Could not found requested data", 404));
   }
 
   @ExceptionHandler({IllegalAccessException.class})
-  public ResponseEntity<ErrorResponse> handleIllegalAccess(IllegalAccessException e) {
-    logger.warn(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleIllegalAccess(IllegalAccessException exception) {
+    logger.warn(exception.getMessage());
     return ResponseEntity.status(HttpStatus.FORBIDDEN)
         .body(new ErrorResponse("Access denied.", 403));
   }
 
   @ExceptionHandler({IllegalArgumentException.class})
-  public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException e) {
-    logger.warn(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException exception) {
+    logger.warn(exception.getMessage());
     return ResponseEntity.badRequest()
         .body(new ErrorResponse("Bad request.", 400));
   }
