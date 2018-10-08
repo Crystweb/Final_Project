e794a982255bdeddf97e3ee0e3689f220eed2180
Dmytro <vlasg1979>
2018-10-08 14:58:27 +0300

    save after set vacancy to state

--- src/main/java/ua/danit/final_project/entities/Vacancy.java
+++ src/main/java/ua/danit/final_project/entities/Vacancy.java
@@ -6,18 +6,10 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
 @Table(name = "vacancy")
@@ -53,6 +45,11 @@ public class Vacancy implements Serializable {
   @JsonFormat(pattern = "dd-MM-yyyy hh:mm")
   @Column(name = "p_publication", nullable = false)
   private Timestamp publication;
+
+  @ToString.Exclude
+  @EqualsAndHashCode.Exclude
+  @OneToMany(mappedBy = "vacancy", cascade = CascadeType.ALL)
+  private List<VacancyComment> comments;
 
   public enum VacancyStatus {
     OPENED, CLOSED
