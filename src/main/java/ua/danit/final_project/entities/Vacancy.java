cbc3baa54b5d379980c72b782f30323e9581fdf4
Dmytro <vlasg1979>
2018-10-08 15:20:28 +0300

    save after set vacancy to state

--- src/main/java/ua/danit/final_project/entities/Vacancy.java
+++ src/main/java/ua/danit/final_project/entities/Vacancy.java
@@ -5,8 +5,18 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.List;
