4fbc1d06f4c4c576d93e56e98ebaf89a83e16ec1
Vlas <40273797+VlasD@users.noreply.github.com>
2018-10-08 16:09:31 +0300

    Vacancy edit (#144)
    * save after set vacancy to state
    
    * save after set vacancy to state
    
    * save after set vacancy to state

--- src/main/java/ua/danit/final_project/entities/Vacancy.java
+++ src/main/java/ua/danit/final_project/entities/Vacancy.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-
 import javax.persistence.Column;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -15,9 +15,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
 @Table(name = "vacancy")
@@ -53,6 +55,11 @@ public class Vacancy implements Serializable {
   @JsonFormat(pattern = "dd-MM-yyyy hh:mm")
   @Column(name = "p_publication", nullable = false)
   private Timestamp publication;
+
+  @ToString.Exclude
+  @EqualsAndHashCode.Exclude
+  @OneToMany(mappedBy = "vacancy", cascade = CascadeType.ALL)
+  private List<VacancyComment> comments;
 
   public enum VacancyStatus {
     OPENED, CLOSED
