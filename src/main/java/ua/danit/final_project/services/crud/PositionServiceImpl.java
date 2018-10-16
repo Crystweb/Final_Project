b54a9b67779584ffdf20e7f0c0536f4332778fcb
Rostyslav Barmakov <warfawl@gmail.com>
2018-10-16 20:07:56 +0300

    refactored DTO an services

--- src/main/java/ua/danit/final_project/services/crud/PositionServiceImpl.java
+++ src/main/java/ua/danit/final_project/services/crud/PositionServiceImpl.java
@@ -2,35 +2,48 @@ package ua.danit.final_project.services.crud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ua.danit.final_project.dto.DefaultMapper;
+import ua.danit.final_project.dto.PositionDto;
 import ua.danit.final_project.entities.Position;
 import ua.danit.final_project.repositories.PositionRepository;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PositionServiceImpl implements PositionService{
 
   private final PositionRepository positionRepository;
+  private final DefaultMapper mapper;
 
   @Autowired
-  public PositionServiceImpl(PositionRepository positionRepository) {
+  public PositionServiceImpl(PositionRepository positionRepository,
+                             DefaultMapper mapper) {
     this.positionRepository = positionRepository;
+    this.mapper = mapper;
   }
 
   @Override
-  public Position getById(Long id) {
-    return positionRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+  public PositionDto getById(Long id) {
+    return positionRepository.findById(id)
+        .map(mapper::positionToPositionDto)
+        .orElseThrow(EntityNotFoundException::new);
   }
 
   @Override
-  public List<Position> getAll() {
-    return positionRepository.findAll();
+  public List<PositionDto> getAll() {
+    return positionRepository.findAll()
+        .stream()
+        .map(mapper::positionToPositionDto)
+        .collect(Collectors.toList());
   }
 
   @Override
-  public Position save(Position position) {
-    return positionRepository.save(position);
+  public PositionDto save(PositionDto positionDto) {
+    Position position = mapper.positionDtoToPosition(positionDto);
+    position = positionRepository.save(position);
+    return mapper.positionToPositionDto(position);
   }
 
   @Override
@@ -39,7 +52,17 @@ public class PositionServiceImpl implements PositionService{
   }
 
   @Override
-  public List<Position> findAll() {
-    return positionRepository.findAll();
+  public List<PositionDto> findAll() {
+    return positionRepository.findAll()
+        .stream()
+        .map(mapper::positionToPositionDto)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public List<PositionDto> getAllPinnedToComment() {
+    return findAll().stream()
+        .filter(PositionDto::getPinnedToComment)
+        .collect(Collectors.toList());
   }
 }
--- src/main/java/ua/danit/final_project/services/crud/VacancyServiceCrudImpl.java
+++ src/main/java/ua/danit/final_project/services/crud/VacancyServiceCrudImpl.java
@@ -2,92 +2,59 @@ package ua.danit.final_project.services.crud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ua.danit.final_project.entities.Position;
-import ua.danit.final_project.entities.User;
+import ua.danit.final_project.dto.DefaultMapper;
+import ua.danit.final_project.dto.VacancyDto;
 import ua.danit.final_project.entities.Vacancy;
-import ua.danit.final_project.repositories.PositionRepository;
-import ua.danit.final_project.repositories.UserRepository;
 import ua.danit.final_project.repositories.VacancyRepository;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
 import java.sql.Timestamp;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class VacancyServiceCrudImpl implements VacancyServiceCrud {
   private final VacancyRepository vacancyRepository;
-  private final PositionRepository positionRepository;
-  private final UserRepository userRepository;
+  private final DefaultMapper mapper;
 
   @Autowired
   public VacancyServiceCrudImpl(VacancyRepository vacancyRepository,
-                                PositionRepository positionRepository,
-                                UserRepository userRepository) {
+                                DefaultMapper mapper) {
     this.vacancyRepository = vacancyRepository;
-    this.positionRepository = positionRepository;
-    this.userRepository = userRepository;
+    this.mapper = mapper;
   }
 
   @Override
-  public Vacancy getById(Long id) {
-    return vacancyRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+  public VacancyDto getById(Long id) {
+    return vacancyRepository.findById(id)
+        .map(mapper::vacancyToVacancyDto)
+        .orElseThrow(EntityNotFoundException::new);
   }
 
   @Override
-  public List<Vacancy> getOpenVacancies() {
+  public List<VacancyDto> getOpenVacancies() {
     return vacancyRepository.findAll().stream()
+        .map(mapper::vacancyToVacancyDto)
         .collect(Collectors.toList());
   }
 
   @Override
-  public Vacancy save(Vacancy vacancy) {
-    vacancy.setPublication(new Timestamp(System.currentTimeMillis()));
-    return vacancyRepository.save(vacancy);
+  public VacancyDto create(VacancyDto vacancyDto) {
+    vacancyDto.setPublication(new Timestamp(System.currentTimeMillis()));
+    Vacancy vacancy = mapper.vacancyDtoToVacancy(vacancyDto);
+    vacancy = vacancyRepository.save(vacancy);
+    return mapper.vacancyToVacancyDto(vacancy);
   }
 
   @Override
-  public Vacancy updateVacancy(Vacancy vacancy, User userFromToken) throws IllegalAccessException {
-
-    if (!vacancy.getUser().getId().equals(userFromToken.getId())) {
-      String message = "User dont have permission to update this vacancy - id: " + vacancy.getId();
-      throw new IllegalAccessException(message);
-    }
-
-    return vacancyRepository.save(vacancy);
+  public VacancyDto updateVacancy(VacancyDto vacancyDto) {
+    Vacancy vacancy = mapper.vacancyDtoToVacancy(vacancyDto);
+    vacancy = vacancyRepository.save(vacancy);
+    return mapper.vacancyToVacancyDto(vacancy);
   }
 
   @Override
-  public void deleteVacancy(Vacancy vacancy, User userFromToken) throws IllegalAccessException {
-
-    if (!vacancy.getUser().getId().equals(userFromToken.getId())) {
-      String message = "User dont have permission to delete this vacancy - id: " + vacancy.getId();
-      throw new IllegalAccessException(message);
-    }
-
+  public void deleteVacancy(Vacancy vacancy) {
     vacancyRepository.delete(vacancy);
   }
-
-  @Override
-  public Position getPositionByTitle(String title) {
-    return positionRepository.getPositionByTitle(title);
-  }
-
-  @Override
-  public User getUserByid(Long userId) {
-    return userRepository.getOne(userId);
-  }
-
-  @Override
-  public Position createIfNotExist(String title) {
-    if (positionRepository.existsByTitleEquals(title)) {
-      return getPositionByTitle(title);
-    } else {
-      Position createPosition = new Position();
-      createPosition.setTitle(title);
-      return positionRepository.save(createPosition);
-    }
-  }
-
-
 }
