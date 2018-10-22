8d3052398b5e8a3e24816d1fcffb1f6ee481b183
Artem <artyomtymchuk@gmail.com>
2018-10-22 21:30:30 +0300

    Merge branch 'master' of https://github.com/Romik9/Final_Project into addTasks
    # Conflicts:
    #	client/src/components/Navigation.js
    #	client/src/constants/routes.js
    #	client/src/pages/VacancyServicePage.js
    #	src/main/java/ua/danit/final_project/controllers/TaskController.java
    #	src/main/java/ua/danit/final_project/services/tasks/TaskService.java

--- src/main/java/ua/danit/final_project/entities/Task.java
+++ src/main/java/ua/danit/final_project/entities/Task.java
@@ -5,7 +5,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
