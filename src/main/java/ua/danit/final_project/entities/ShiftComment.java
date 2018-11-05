0d1c219850a8c495bbe8077e25ce2f29e8871315
Rostyslav Barmakov <warfawl@gmail.com>
2018-11-05 19:10:46 +0200

    task and employee fix

--- src/main/java/ua/danit/final_project/entities/ShiftComment.java
+++ src/main/java/ua/danit/final_project/entities/ShiftComment.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
