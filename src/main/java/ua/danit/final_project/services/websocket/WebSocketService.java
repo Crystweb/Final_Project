4e565bac7fdd0d2029160593bf0511328e76b2e2
Rostyslav Barmakov <warfawl@gmail.com>
2018-11-06 17:35:06 +0200

    added web socket

---
+++ src/main/java/ua/danit/final_project/services/websocket/WebSocketService.java
@@ -0,0 +1,16 @@
+package ua.danit.final_project.services.websocket;
+
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Service;
+import ua.danit.final_project.configuration.SessionAware;
+
+@Service
+public class WebSocketService extends SessionAware {
+
+  private final String WS_PATH = "/events";
+
+  @SendTo(WS_PATH)
+  public String updateTask(String json) {
+    return json;
+  }
+}
