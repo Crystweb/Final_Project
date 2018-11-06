004f70c73e18fca7989344cbc4b7e7c679fa9382
Rostyslav Barmakov <warfawl@gmail.com>
2018-11-06 15:21:17 +0200

    removed unused import

--- src/main/java/ua/danit/final_project/services/storage/StorageService.java
+++ src/main/java/ua/danit/final_project/services/storage/StorageService.java
@@ -1,7 +1,6 @@
 package ua.danit.final_project.services.storage;
 
 import org.springframework.web.multipart.MultipartFile;
-import ua.danit.final_project.entities.AbstractImage;
 import ua.danit.final_project.entities.Employee;
 import ua.danit.final_project.entities.EmployeeImage;
 import ua.danit.final_project.entities.Task;
