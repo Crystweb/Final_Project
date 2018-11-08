fcacdbc37b5e72a2d9287e97de114f9d250c4871
Rostyslav Barmakov <warfawl@gmail.com>
2018-11-08 23:47:47 +0200

    code style fix

--- src/main/java/ua/danit/final_project/configuration/security/JwtTokenUtil.java
+++ src/main/java/ua/danit/final_project/configuration/security/JwtTokenUtil.java
@@ -55,8 +55,8 @@ public class JwtTokenUtil {
   }
 
   private Boolean isTokenExpired(String token) {
-    final Date expiration = getExpirationDateFromToken(token);
-    return expiration.before(clock.now());
+    final Date tokenExpiration = getExpirationDateFromToken(token);
+    return tokenExpiration.before(clock.now());
   }
 
   private Boolean isCreatedBeforeLastPasswordReset(Date created, Date lastPasswordReset) {
